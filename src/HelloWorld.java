ae49ffb953208ea46828ae89f3a6721e344b303d
arianne19892020 <arianne.amalfitani@hotmail.com>
2022-11-28 14:24:19 -0300

    Commit1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,8 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hola Mundo");
 	}
 
 }
